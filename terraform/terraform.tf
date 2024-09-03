d4f15d02bd1017969db52b4c6a0ed0aaf6362495
linjiansi <pmon.ken.1467@gmail.com>
2024-09-03 13:29:53 +0900

    Add terraform conf

---
+++ terraform/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "1.9.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "sample-template-terraform-state"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+    encrypt = true
+
+    dynamodb_table = "sample-template-terraform-state-lock"
+  }
+}
